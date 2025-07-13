d81dfadfa4984febe6527d4bb0a92d4806a89bd1
Lombardo Moreno Rodríguez <LombardoCode@gmail.com>
2025-07-13 08:40:05 -0600

    Dashboard: Implement nested routing layout with sidebar and dynamic content area.

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,10 +1,6 @@
 'use client';
 
 import Text from "../components/UIComponents/Text";
-import DashboardOutletContainer from "../components/utils/DashboardOutletContainer";
-import DashboardOutletNavbar from "../components/utils/DashboardOutletNavbar";
-import MainContent from "../components/utils/MainContent";
-import SidebarNav from "../components/utils/SidebarNav";
 import { FontWeights, TextColors, UseCases } from "../interfaces/Text.interface";
 import { useEffect, useState } from "react";
 import axiosInstance from "../utils/axiosConfig";
@@ -31,64 +27,58 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="flex h-screen bg-red-500 overflow-hidden">
-      <SidebarNav className="min-w-60 bg-white h-full" />
-      <MainContent topSpacing={false} className="flex-1 bg-white overflow-y-scroll">
-        <DashboardOutletNavbar />
-        <DashboardOutletContainer>
+    <>
+      <Text
+        fontWeight={FontWeights.BOLD}
+        useCase={UseCases.HEADER}
+        textColor={TextColors.BLACK}
+        className="text-3xl"
+      >
+        Your kahoots
+      </Text>
+
+      {kahootsFromUser.length === 0
+        ? (
           <Text
             fontWeight={FontWeights.BOLD}
-            useCase={UseCases.HEADER}
             textColor={TextColors.BLACK}
-            className="text-3xl"
+            useCase={UseCases.LONGTEXT}
           >
-            Your kahoots
+            You don&apos;t have any kahoots for now. Create your own Kahoot now!
           </Text>
-
-          {kahootsFromUser.length === 0
-            ? (
-              <Text
-                fontWeight={FontWeights.BOLD}
-                textColor={TextColors.BLACK}
-                useCase={UseCases.LONGTEXT}
+        )
+        : (
+          <div id="kahoots-created-by-user" className="my-4 grid grid-cols-4 gap-4">
+            {kahootsFromUser.map((kahoot: KahootDashboardList, index: number) => (
+              <div
+                key={index}
+                className="bg-red-500"
               >
-                You don&apos;t have any kahoots for now. Create your own Kahoot now!
-              </Text>
-            )
-            : (
-              <div id="kahoots-created-by-user" className="my-4 grid grid-cols-4 gap-4">
-                {kahootsFromUser.map((kahoot: KahootDashboardList, index: number) => (
-                  <div
-                    key={index}
-                    className="bg-red-500"
+                <Card
+                  className="cursor-pointer hover:bg-zinc-300"
+                  onClick={() => router.push(`/creator/${kahoot.id}`)}
+                >
+                  <Text
+                    fontWeight={FontWeights.BOLD}
+                    textColor={TextColors.BLACK}
+                    useCase={UseCases.LONGTEXT}
                   >
-                    <Card
-                      className="cursor-pointer hover:bg-zinc-300"
-                      onClick={() => router.push(`/creator/${kahoot.id}`)}
-                    >
-                      <Text
-                        fontWeight={FontWeights.BOLD}
-                        textColor={TextColors.BLACK}
-                        useCase={UseCases.LONGTEXT}
-                      >
-                        {kahoot.title}
-                      </Text>
+                    {kahoot.title}
+                  </Text>
 
-                      <Text
-                        fontWeight={FontWeights.REGULAR}
-                        textColor={TextColors.BLACK}
-                        useCase={UseCases.LONGTEXT}
-                      >
-                        {kahoot.description}
-                      </Text>
-                    </Card>
-                  </div>
-                ))}
+                  <Text
+                    fontWeight={FontWeights.REGULAR}
+                    textColor={TextColors.BLACK}
+                    useCase={UseCases.LONGTEXT}
+                  >
+                    {kahoot.description}
+                  </Text>
+                </Card>
               </div>
-            )}
-        </DashboardOutletContainer>
-      </MainContent>
-    </div>
+            ))}
+          </div>
+        )}
+    </>
   )
 }
 
