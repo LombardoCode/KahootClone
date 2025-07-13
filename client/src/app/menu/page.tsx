2c99bc7a8eedb819a4f075fac10e3601663f174f
Lombardo Moreno Rodríguez <LombardoCode@gmail.com>
2025-07-13 13:04:46 -0600

    Redirecting the user to '/menu/discovery' when visiting '/menu'.

--- client/src/app/menu/page.tsx
+++ client/src/app/menu/page.tsx
@@ -1,85 +1,9 @@
 'use client';
 
-import Text from "../components/UIComponents/Text";
-import { FontWeights, TextColors, UseCases } from "../interfaces/Text.interface";
-import { useEffect, useState } from "react";
-import axiosInstance from "../utils/axiosConfig";
-import { useRouter } from "next/navigation";
-import { KahootDashboardList } from "../interfaces/Kahoot/Dashboard/KahootDashboardList.interface";
-import Card from "../components/UIComponents/Card";
+import { redirect } from "next/navigation";
 
 const DiscoveryMenuPage = () => {
-  const router = useRouter();
-  const [kahootsFromUser, setKahootsFromUser] = useState<KahootDashboardList[]>([]);
-
-  useEffect(() => {
-    getBasicInfoFromUsersKahoots();
-  }, []);
-
-  const getBasicInfoFromUsersKahoots = async () => {
-    await axiosInstance.get('/kahoot/getBasicInfoFromUsersKahoots')
-      .then((res) => {
-        setKahootsFromUser(res.data);
-      })
-      .catch(err => {
-        console.error(err);
-      })
-  }
-
-  return (
-    <>
-      <Text
-        fontWeight={FontWeights.BOLD}
-        useCase={UseCases.HEADER}
-        textColor={TextColors.BLACK}
-        className="text-3xl"
-      >
-        Your kahoots
-      </Text>
-
-      {kahootsFromUser.length === 0
-        ? (
-          <Text
-            fontWeight={FontWeights.BOLD}
-            textColor={TextColors.BLACK}
-            useCase={UseCases.LONGTEXT}
-          >
-            You don&apos;t have any kahoots for now. Create your own Kahoot now!
-          </Text>
-        )
-        : (
-          <div id="kahoots-created-by-user" className="my-4 grid grid-cols-4 gap-4">
-            {kahootsFromUser.map((kahoot: KahootDashboardList, index: number) => (
-              <div
-                key={index}
-                className="bg-red-500"
-              >
-                <Card
-                  className="cursor-pointer hover:bg-zinc-300"
-                  onClick={() => router.push(`/creator/${kahoot.id}`)}
-                >
-                  <Text
-                    fontWeight={FontWeights.BOLD}
-                    textColor={TextColors.BLACK}
-                    useCase={UseCases.LONGTEXT}
-                  >
-                    {kahoot.title}
-                  </Text>
-
-                  <Text
-                    fontWeight={FontWeights.REGULAR}
-                    textColor={TextColors.BLACK}
-                    useCase={UseCases.LONGTEXT}
-                  >
-                    {kahoot.description}
-                  </Text>
-                </Card>
-              </div>
-            ))}
-          </div>
-        )}
-    </>
-  )
+  redirect('/menu/discovery');
 }
 
 export default DiscoveryMenuPage;
