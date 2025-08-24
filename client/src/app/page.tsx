a1745e64d8bbae98aa31772d62c923e654bfade0
Lombardo Moreno Rodríguez <LombardoCode@gmail.com>
2025-08-23 23:55:17 -0600

    Root page (UI): A new 'Nickname' entry form is now shown when entering a valid GamePIN (lobbyId).

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -18,26 +18,85 @@ import SpinnerHalfDonut from "./components/UIComponents/Spinners/SpinnerHalfDonu
 import { SpinnerSizes } from "./components/UIComponents/Spinners/Spinner.interface";
 
 const Home = () => {
-  const router = useRouter();
-  const [formData, setFormData] = useState({
-    gamePIN: ''
-  });
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [highlightInput, setHighlightInput] = useState<boolean>(false);
+  const [userEnteredAValidLobbyId, setUserEnteredAValidLobbyId] = useState<boolean>(false);
+  const [gamePIN, setGamePIN] = useState<string>("");
+  const [nickName, setNickName] = useState<string>("");
 
-  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (highlightInput) {
-      setHighlightInput(false);
-    }
+  return (
+    <>
+      <div className="bg-violet-950 w-full h-screen">
+        {loading && (
+          <LoadingKahootScreen />
+        )}
 
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  }
+        <BackgroundShapes shapeColor={ShapeColor.VIOLET} />
+        <div className="flex flex-col h-full">
+          <div className="flex flex-1 justify-center items-center flex-col relative z-10">
+            <Logo
+              size={LogoSize.LARGE}
+              className="mb-4"
+            />
+            <Card
+              backgroundColor={CardBackgroundColors.WHITE}
+              className="min-w-[350px] flex flex-col justify-between"
+            >
+              {!userEnteredAValidLobbyId
+              ? (
+                <ShowEnterLobbyIdFields
+                  highlightInput={highlightInput}
+                  setError={setError}
+                  setHighlightInput={setHighlightInput}
+                  setLoading={setLoading}
+                  setUserEnteredAValidLobbyId={setUserEnteredAValidLobbyId}
+                  gamePIN={gamePIN}
+                  setGamePIN={setGamePIN}
+                />
+              )
+              : (
+                <ShowEnterNicknameFields
+                  highlightInput={highlightInput}
+                  nickName={nickName}
+                  setNickName={setNickName}
+                  gamePIN={gamePIN}
+                />
+              )}
+            </Card>
+          </div>
+          <footer className="text-center relative z-10 py-5">
+            <Text
+              useCase={UseCases.LONGTEXT}
+              textColor={TextColors.WHITE}
+              fontWeight={FontWeights.LIGHT}
+            >
+              Create your own Kahoot account for FREE by clicking <Link href="/signup">here</Link>!
+            </Text>
+          </footer>
+        </div>
 
-  const tryEnterIntoTheGame = async (gamePIN: number | string) => {
+        <ErrorNotifier
+          error={error}
+          setError={setError}
+        />
+      </div>
+    </>
+  );
+}
+
+interface ShowEnterLobbyIdFieldsProps {
+  highlightInput: boolean;
+  setError: (error: string | null) => void;
+  setHighlightInput: (val: boolean) => void;
+  setLoading: (val: boolean) => void;
+  setUserEnteredAValidLobbyId: (val: boolean) => void;
+  gamePIN: string;
+  setGamePIN: (gamePIN: string) => void;
+}
+
+const ShowEnterLobbyIdFields = ({ highlightInput, setError, setHighlightInput, setLoading, setUserEnteredAValidLobbyId, gamePIN, setGamePIN }: ShowEnterLobbyIdFieldsProps) => {
+  const processGamePIN = async () => {
     setError(null);
     setHighlightInput(false);
     const isEmpty: boolean = gamePIN.toString().trim() === "";
@@ -70,7 +129,7 @@ const Home = () => {
       const isAValidLobby: boolean = res.data;
 
       if (isAValidLobby) {
-        enterToTheLobby(gamePIN);
+        setUserEnteredAValidLobbyId(true);
       } else {
         setError("We didn't recognize that game PIN. Please check and try again.");
         setHighlightInput(true);
@@ -81,83 +140,98 @@ const Home = () => {
       setLoading(false);
     }
   }
+  
+  return (
+    <>
+      <InputForm
+        type={InputFormTypes.TEXT}
+        roundness={Roundness.SMALL}
+        textColor={TextColors.GRAY}
+        fontWeight={FontWeights.BOLD}
+        borderSize={BorderSize.SMALL}
+        focusBorderColor={!highlightInput ? FocusBorderColor.BLUE : FocusBorderColor.RED}
+        id="gamePIN"
+        name="gamePIN"
+        className={`outline-none py-3 my-1 text-center ${highlightInput && 'border-kahoot-red-200'}`}
+        placeholder="Game PIN"
+        value={gamePIN}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setGamePIN(e.target.value)}
+        rightElement={
+          highlightInput && (
+            <FontAwesomeIcon
+              icon={faExclamation}
+              className={`text-kahoot-red-200 mr-1 px-3 py-3`}
+              size="xl"
+            />
+          )
+        }
+      />
+      <Button
+        backgroundColor={BackgroundColors.GRAY}
+        textColor={TextColors.WHITE}
+        perspective={PerspectiveSize.MEDIUM}
+        animateOnHover={false}
+        fontWeight={FontWeights.BOLD}
+        className="mt-3"
+        onClick={() => processGamePIN()}
+      >
+        Enter
+      </Button>
+    </>
+  )
+}
 
-  const enterToTheLobby = (gamePIN: string | number) => {
+interface ShowEnterNicknameFieldsProps {
+  highlightInput: boolean;
+  nickName: string;
+  setNickName: (nickname: string) => void;
+  gamePIN: string | number;
+}
+
+const ShowEnterNicknameFields = ({ highlightInput, nickName, setNickName, gamePIN }: ShowEnterNicknameFieldsProps) => {
+  const router = useRouter();
+
+  const enterToTheLobby = () => {
     router.push(`/lobby/${gamePIN}`);
   }
 
   return (
     <>
-      <div className="bg-violet-950 w-full h-screen">
-        {loading && (
-          <LoadingKahootScreen />
-        )}
-
-        <BackgroundShapes shapeColor={ShapeColor.VIOLET} />
-        <div className="flex flex-col h-full">
-          <div className="flex flex-1 justify-center items-center flex-col relative z-10">
-            <Logo
-              size={LogoSize.LARGE}
-              className="mb-4"
+      <InputForm
+        type={InputFormTypes.TEXT}
+        roundness={Roundness.SMALL}
+        textColor={TextColors.GRAY}
+        fontWeight={FontWeights.BOLD}
+        borderSize={BorderSize.SMALL}
+        id="nickName"
+        name="nickName"
+        className={`outline-none py-3 my-1 text-center`}
+        placeholder="Nickname"
+        value={nickName}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNickName(e.target.value)}
+        rightElement={
+          highlightInput && (
+            <FontAwesomeIcon
+              icon={faExclamation}
+              className={`text-kahoot-red-200 mr-1 px-3 py-3`}
+              size="xl"
             />
-            <Card
-              backgroundColor={CardBackgroundColors.WHITE}
-              className="min-w-[350px] flex flex-col justify-between"
-            >
-              <InputForm
-                type={InputFormTypes.TEXT}
-                roundness={Roundness.SMALL}
-                textColor={TextColors.GRAY}
-                fontWeight={FontWeights.BOLD}
-                borderSize={BorderSize.SMALL}
-                focusBorderColor={!highlightInput ? FocusBorderColor.BLUE : FocusBorderColor.RED}
-                id="gamePIN"
-                name="gamePIN"
-                className={`outline-none py-3 my-1 text-center ${highlightInput && 'border-kahoot-red-200'}`}
-                placeholder="Game PIN"
-                value={formData.gamePIN}
-                onChange={handleFormChange}
-                rightElement={
-                  highlightInput && (
-                    <FontAwesomeIcon
-                      icon={faExclamation}
-                      className={`text-kahoot-red-200 mr-1 px-3 py-3`}
-                      size="xl"
-                    />
-                  )
-                }
-              />
-              <Button
-                backgroundColor={BackgroundColors.GRAY}
-                textColor={TextColors.WHITE}
-                perspective={PerspectiveSize.MEDIUM}
-                animateOnHover={false}
-                fontWeight={FontWeights.BOLD}
-                className="mt-3"
-                onClick={() => tryEnterIntoTheGame(formData.gamePIN)}
-              >
-                Enter
-              </Button>
-            </Card>
-          </div>
-          <footer className="text-center relative z-10 py-5">
-            <Text
-              useCase={UseCases.LONGTEXT}
-              textColor={TextColors.WHITE}
-              fontWeight={FontWeights.LIGHT}
-            >
-              Create your own Kahoot account for FREE by clicking <Link href="/signup">here</Link>!
-            </Text>
-          </footer>
-        </div>
-
-        <ErrorNotifier
-          error={error}
-          setError={setError}
-        />
-      </div>
+          )
+        }
+      />
+      <Button
+        backgroundColor={BackgroundColors.GRAY}
+        textColor={TextColors.WHITE}
+        perspective={PerspectiveSize.MEDIUM}
+        animateOnHover={false}
+        fontWeight={FontWeights.BOLD}
+        className="mt-3"
+        onClick={() => enterToTheLobby()}
+      >
+        Enter
+      </Button>
     </>
-  );
+  )
 }
 
 const LoadingKahootScreen = () => {
