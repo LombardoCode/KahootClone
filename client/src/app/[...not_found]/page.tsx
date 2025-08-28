6708772e7ad917d7823b5240fedce10a45cc0815
Lombardo Moreno Rodríguez <LombardoCode@gmail.com>
2025-08-28 16:39:58 -0600

    Routing (Not found): Redirect to the Discover path whenever a logged-in user tries to access a non-existing path.

---
+++ client/src/app/[...not_found]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ROUTES } from "../utils/Routes/routesUtils";
+import { useRouter } from "next/navigation";
+
+const NotFoundPage = () => {
+  // At this point of the execution, user must be logged-in because they succeeded the middleware.ts filter, so we don't need to check if they are logged-in.
+  const router = useRouter();
+  
+  // Just send the logged-in user to the dashboard whenever they try to access a non-existing path.
+  router.push(ROUTES.MENU.DISCOVERY);
+}
+
+export default NotFoundPage;
